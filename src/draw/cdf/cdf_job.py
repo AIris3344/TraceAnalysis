8adb2dfea30a1339a2fef451b55e3ce3d5194d4f
airis <airis@airisdeMacBook-Pro.local>
2019-08-23 17:03:14 +0100

    add cdf

--- src/draw/cdf/cdf_job.py
+++ src/draw/cdf/cdf_job.py
@@ -11,24 +11,9 @@ import os
 data_path = "hdfs://10.1.4.11:9000/user/hduser/"
 local_path = os.environ['HOME'] + "/data/"
 
-spark = SparkSession.builder     .master("local[*]")     .appName("TraceAnalysis")     .config("spark.driver.memory", "8g")     .getOrCreate()
+spark = SparkSession.builder     .master("local[*]")     .appName("TraceAnalysis")     .config("spark.driver.memory", "10g")     .getOrCreate()
 
 #%%
-# Read batck_task parquet
-df_batch_task = spark    .read    .parquet(data_path + "batch_task_parquet")
-print("batch_task:")
-df_batch_task.show()
-df_batch_task.createOrReplaceTempView("batch_task")
-
-# Read batch_instance parquet
-df_batch_instance = spark    .read    .parquet(data_path + "batch_instance_parquet")
-print("batch_instance:")
-df_batch_instance.show()
-df_batch_instance.createOrReplaceTempView("batch_instance")
-
-#%%
-######################## Write staging results to HDFS ####################################
-
 # Read staging results
 duration = spark.read.parquet(local_path + "batch_task_staging/job_duration_parquet").filter("duration >= 0")
 
@@ -50,7 +35,7 @@ plt.vlines(x_99, 0, 0.99, colors="r", linestyles="dashed", label="99% job")
 plt.text(x_99, 0.99, str(x_99), size=10, position=(x_99, 0.5))
 
 #%%
-duration = spark.read.parquet(local_path + "batch_instance_staging/task_duration_parquet").filter("duration >= 0")
+duration = spark.read.parquet(data_path + "batch_instance_staging/task_duration_parquet").filter("duration >= 0")
 
 # Reshape the list to 1-d array
 rows = duration.count()
@@ -71,7 +56,7 @@ plt.text(x_99, 0.99, str(x_99), size=10, position=(x_99, 0.5))
 
 #%%
 duration = spark.read.parquet(local_path + "batch_instance_staging/instance_duration_parquet").filter("duration >= 0")
-
+#%%
 # Reshape the list to 1-d array
 rows = duration.count()
 data = np.reshape(duration.select("duration").collect(), rows)
