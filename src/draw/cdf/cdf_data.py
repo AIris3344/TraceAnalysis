11a9c6f410531d1a2a483c42cdf051a7a8e47cc4
airis <airis@airisdeMacBook-Pro.local>
2019-08-23 17:05:54 +0100

    add data to hdfs

--- src/draw/cdf/cdf_data.py
+++ src/draw/cdf/cdf_data.py
@@ -20,10 +20,10 @@ df_batch_task.write.text(data_path + "batch_task_staging/job_duration.txt")
 # 
 # # Write task total time to staging results
 df_batch_task = spark.sql("SELECT SUM(end_time - start_time) AS duration FROM batch_instance GROUP BY task_name")
-df_batch_task.write.parquet(data_path + "batch_instance_staging/task_duration.txt")
+df_batch_task.write.text(data_path + "batch_instance_staging/task_duration.txt")
 # 
 # # Write instance total time to staging results
 df_batch_instance = spark.sql("SELECT SUM(end_time - start_time) AS duration FROM batch_instance GROUP BY instance_name")
-df_batch_instance.write.parquet(data_path + "batch_instance_staging/instance_duration.txt")
+df_batch_instance.write.text(data_path + "batch_instance_staging/instance_duration.txt")
 
 ###########################################################################################
