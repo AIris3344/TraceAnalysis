bc3e1fc4825f55b6bf2cb039e5f3862d3f5888e1
airis <airis@airisdeMacBook-Pro.local>
2019-08-23 17:05:28 +0100

    add data to hdfs

--- src/draw/cdf/cdf_data.py
+++ src/draw/cdf/cdf_data.py
@@ -16,14 +16,14 @@ df_batch_instance.createOrReplaceTempView("batch_instance")
 
 # # Write job total time to staging results
 df_batch_task = spark.sql("SELECT SUM(end_time - start_time) AS duration FROM batch_task GROUP BY job_name")
-df_batch_task.write.parquet(data_path + "batch_task_staging/job_duration_parquet")
+df_batch_task.write.text(data_path + "batch_task_staging/job_duration.txt")
 # 
 # # Write task total time to staging results
-df_batch_task = spark.sql("SELECT task_name, SUM(end_time - start_time) AS duration FROM batch_instance GROUP BY task_name")
-df_batch_task.write.parquet(data_path + "batch_instance_staging/task_duration_parquet")
+df_batch_task = spark.sql("SELECT SUM(end_time - start_time) AS duration FROM batch_instance GROUP BY task_name")
+df_batch_task.write.parquet(data_path + "batch_instance_staging/task_duration.txt")
 # 
 # # Write instance total time to staging results
-df_batch_instance = spark.sql("SELECT instance_name, SUM(end_time - start_time) AS duration FROM batch_instance GROUP BY instance_name")
-df_batch_instance.write.parquet(data_path + "batch_instance_staging/instance_duration_parquet")
+df_batch_instance = spark.sql("SELECT SUM(end_time - start_time) AS duration FROM batch_instance GROUP BY instance_name")
+df_batch_instance.write.parquet(data_path + "batch_instance_staging/instance_duration.txt")
 
 ###########################################################################################
