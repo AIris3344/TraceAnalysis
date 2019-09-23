f4ba466812f2dd15e0819d7c084c22674110c46e
Aris <aris@codesigned.co.uk>
2019-09-23 22:40:51 +0100

    Commit architecture description

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # TraceAnalysis
 This is a project for data analysis based on Alibaba trace of 2018 (https://github.com/alibaba/clusterdata/tree/master/cluster-trace-v2018).
+
+#
+The architecutre of this project is shown below:
+## src
+### main
+This includes some test and initial work.
+### conver-parquet
+This includes the process of converting csv to parquet format.
+### draw
+This includes the logic of drawing graphs using Matplotlib.
+### staging
+This calculate some staging results for the processing.
+
+#
+## img
+This includes the graphs drawn by the project.
--- src/draw/performance/container_cpu_cpi.py
+++ src/draw/performance/container_cpu_cpi.py
@@ -1,3 +1,4 @@
+## Thif file is to draw different patterns of CPI and CPU in cluster
 #%%
 import os
 import numpy as np
@@ -20,31 +21,30 @@ usage = spark.read.parquet(data_path + "container_usage_parquet")
 # usage.show()
 usage.createOrReplaceTempView("usage")
 
-## Machine
-# machine_usage = spark.sql("SELECT machine_id, AVG(cpu_util_percent) AS cpu, AVG(mem_util_percent) AS mem, AVG(cpi) AS cpi, AVG(mpki) AS mpki FROM (SELECT CAST(SUBSTRING(machine_id, 3, 20) AS INT) AS machine_id, cpu_util_percent, mem_util_percent, cpi, mpki FROM usage) t GROUP BY machine_id ORDER BY machine_id")
-# machine_usage.write.parquet(write_path + "usage_machine_parquet")
+# Machine
+machine_usage = spark.sql("SELECT machine_id, AVG(cpu_util_percent) AS cpu, AVG(mem_util_percent) AS mem, AVG(cpi) AS cpi, AVG(mpki) AS mpki FROM (SELECT CAST(SUBSTRING(machine_id, 3, 20) AS INT) AS machine_id, cpu_util_percent, mem_util_percent, cpi, mpki FROM usage) t GROUP BY machine_id ORDER BY machine_id")
+machine_usage.write.parquet(write_path + "usage_machine_parquet")
 
-## CPU
-# meta = spark.sql("SELECT container_id, AVG(cpu_request) / 100 AS cpu_request, AVG(cpu_limit) / 100 AS cpu_limit FROM (SELECT CAST(SUBSTRING(container_id, 3, 20) AS INT) AS container_id, cpu_request, cpu_limit FROM meta) t GROUP BY container_id ORDER BY container_id")
-# meta.show()
-# meta.createOrReplaceTempView("meta")
+# CPU
+meta = spark.sql("SELECT container_id, AVG(cpu_request) / 100 AS cpu_request, AVG(cpu_limit) / 100 AS cpu_limit FROM (SELECT CAST(SUBSTRING(container_id, 3, 20) AS INT) AS container_id, cpu_request, cpu_limit FROM meta) t GROUP BY container_id ORDER BY container_id")
+meta.show()
+meta.createOrReplaceTempView("meta")
 
-# result = spark.sql("SELECT t1.container_id, (t1.cpu * t2.cpu_request) / 100 AS cpu, t2.cpu_request, t2.cpu_limit FROM usage t1 JOIN meta t2 ON t1.container_id = t2.container_id ORDER BY t1.container_id")
-# result.show()
-# x = np.reshape(result.select("container_id").collect(), -1)
+result = spark.sql("SELECT t1.container_id, (t1.cpu * t2.cpu_request) / 100 AS cpu, t2.cpu_request, t2.cpu_limit FROM usage t1 JOIN meta t2 ON t1.container_id = t2.container_id ORDER BY t1.container_id")
+result.show()
+x = np.reshape(result.select("container_id").collect(), -1)
 
-# fig, ax = plt.subplots(figsize=(10, 5))
-# ax.set_ylabel("CPU")
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.set_ylabel("CPU")
 
-# ax.plot(x, np.reshape(result.select("cpu").collect(), -1), color="r", label="CPU utilization")
-# ax.plot(x, np.reshape(result.select("cpu_request").collect(), -1), color="g", label="CPU request")
-# ax.plot(x, np.reshape(result.select("cpu_limit").collect(), -1), color="gray", label="CPU limit")
+ax.plot(x, np.reshape(result.select("cpu").collect(), -1), color="r", label="CPU utilization")
+ax.plot(x, np.reshape(result.select("cpu_request").collect(), -1), color="g", label="CPU request")
+ax.plot(x, np.reshape(result.select("cpu_limit").collect(), -1), color="gray", label="CPU limit")
 
-# plt.legend()
-# plt.show()
+plt.legend()
+plt.show()
 
 #%%
-"""
 ## Container
 # usage = spark.sql("SELECT container_id, AVG(cpu_util_percent) AS cpu, AVG(mem_util_percent) AS mem, AVG(cpi) AS cpi, AVG(mpki) AS mpki FROM (SELECT CAST(SUBSTRING(container_id, 3, 20) AS INT) AS container_id, cpu_util_percent, mem_util_percent, cpi, mpki FROM usage) t GROUP BY container_id ORDER BY container_id")
 # usage.write.parquet(write_path + "usage_parquet")
@@ -81,6 +81,3 @@ values = np.reshape(cpu_result.select("cpi").collect(), -1)
 ax.bar(range(len(values)), values, tick_label=name_list)
 
 plt.show()
-
-#%%
-"""
